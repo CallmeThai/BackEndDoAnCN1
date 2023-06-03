08844fd7319b8925c2de2b2ddb02e0d0fdf94319
vux <vux.0511@gmail.com>
2023-06-03 23:48:36 +0700

    complete cart

--- api/setOrder.php
+++ api/setOrder.php
@@ -5,35 +5,55 @@
     $data = json_decode(file_get_contents('php://input'), true);
 
     $idUser =(int) $data["idUser"]; 
+    $products = $data["products"]; 
     $fullname = $data["fullname"]; 
     $address = $data["address"]; 
     $phone = $data["phone"]; 
+    $email = $data["email"]; 
     $status = $data["status"]; 
     $payment = $data["payment"]; 
     $totalPrice = $data["totalPrice"];
 
-    $sqlSelect = "SELECT * FROM cart WHERE idUser = '$idUser' AND idProduct = '$idProduct'";
+    if(isset($data["idUser"])) {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $dateOrder = (date("Y-m-d H:i:s"));
+        $sql = "INSERT INTO `order`(`idUser`, `dateOrder`, `fullname`, `address`, `phone`,  `email`, `status`, `payment`, `totalPrice`) VALUES ($idUser ,'$dateOrder' , '$fullname' , '$address' , '$phone', '$email', 'Chờ vận chuyển', 'COD', '$totalPrice')";
+        $result = $conn->query($sql);
     
-    $sql = "INSERT INTO order (`idUser`, `fullname`, `address`, `phone`, `status`, `payment`, `totalPrice`) VALUES ($idUser, $idProduct, 1)";
+        if($result) {
+            $sql = "SELECT * FROM `order` WHERE idUser = '$idUser' AND dateOrder = '$dateOrder'";
+            $resultSelectOne = $conn->query($sql);
     
-    $result = $conn->query($sqlSelect);
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $quantityProductCart = $row['quantityProductCart'];
+            while ($row = $resultSelectOne->fetch_assoc()) {
+                $idOrder = $row['idOrder'];
+            }
+    
+            foreach ($products as  $value) {
+                $idProduct = $value['idProduct'] ;
+                $quantityProduct = $value['quantityProductCart'];
+    
+                $sql = "INSERT INTO detailOrder (`idOrder`, `idProduct`, `quantityProduct`) VALUES ($idOrder, '$idProduct', '$quantityProduct')";
+                $result = $conn->query($sql);
+    
+                if($result) {
+                    $sqlDeleteCart = "DELETE FROM cart WHERE idUser = $idUser";
+                    $resultDeleteCart = $conn->query($sqlDeleteCart); 
+                    if($resultDeleteCart) {
+                        echo json_encode(array(
+                            "message" => "Đặt hàng thành công, chuyển hướng sau 3 giây",
+                            "status" => "success",
+                        ));
+                    }                
+                } else {
+                    echo json_encode(array(
+                        "message" => "Đặt hàng thất bại",
+                        "status" => "fail",
+                    ));
+                }
+            }
         }
-        $sqlUpdate = "UPDATE cart SET quantityProductCart = '$quantityProductCart' + 1 WHERE idUser = '$idUser' AND idProduct = '$idProduct'";
-        $conn->query($sqlUpdate);
-    
-
-    
-    } else {
-        $sql = "INSERT INTO cart (`idUser`, `idProduct`, `quantityProductCart`) VALUES ($idUser, $idProduct, 1)";
-        $conn->query($sql);
-        
-
     }
 
     
-
     $conn->close();
 ?>
