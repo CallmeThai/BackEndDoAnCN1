450129e31f46f8c382c91890c666f4f89304c959
vux <vux.0511@gmail.com>
2023-06-12 00:47:40 +0700

    api user + order

---
+++ api/getCartByUser.php
@@ -0,0 +1,41 @@
+<?php
+
+    include "DBConfig.php";
+
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    if(isset($data["idUser"])) {
+        $idUser =(int) $data["idUser"]; 
+
+        $sqlSelectOrders = "SELECT * FROM `order` WHERE idUser = $idUser";
+        $resultSelectOrders = $conn->query($sqlSelectOrders);
+
+        $orders = array();
+
+        if ($resultSelectOrders->num_rows > 0) {
+            while ($row = $resultSelectOrders->fetch_assoc()) {
+                $order = array(
+                "idOrder" => $row["idOrder"],
+                "idUser" => $row["idUser"],
+                "dateOrder" => $row["dateOrder"],
+                "fullname" => $row["fullname"],
+                "address" => $row["address"],
+                "phone" => $row["phone"],
+                "email" => $row["email"],
+                "payment" => $row["payment"],
+                "status" => $row["status"],
+                "totalPrice" => $row["totalPrice"],
+                );
+                array_push($orders, $order);
+            }
+        }
+
+        echo json_encode($orders);
+    } else {
+        echo "Khong";
+    }
+
+    
+    $conn->close();
+
+?>
